00d8243dd7ab369ee1929b9aa955434d6e44b391
camp5803 <hickerd@sch.ac.kr>
2020-11-06 10:05:19 +0900

    doesn't works

--- chat/app.js
+++ chat/app.js
@@ -1,21 +1,58 @@
-const { Socket } = require('dgram');
+/* express modules */
+const express = require('express');
+const app = express();
 
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+/* http modules */
+const http = require('http');
+const server = http.createServer(app);
+
+/* socket.io modules */
+const socket = require('socket.io');
+const io = socket(server);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end(data);
     });
+});
+
+io.sockets.on('connection', (socket) => {
+    socket.on('newUser', (name) => {
+        console.log(name + ' is connected');
+        socket.name = name;
+        
+        io.sockets.emit('update', { type: 'connect', name: 'SERVER', message: name + ' is connected'});
+    });
+
+    socket.on('message', (data) => {
+        data.name = socket.name;
+        console.log(data);
+
+        socket.broadcast.emit('update', data);
+    });
+
     socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
-http.listen(8080, () => {
-    console.log('Connected at 8080');
+        console.log(name + ' is disconnected');
+
+        socket.broadcast.emit('update', { type: 'disconnect', name: 'SERVER', message: name + ' is disconnected'});
+    })
+})
+
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+})
+
+app.use(function(err, req, res, next) {
+    console.error(err, stack);
+    res.status(500);
+    res.render('500');
+})
+
+app.listen(app.get('port'), function() {
+    console.log( 'Express chatting service is started on http://localhost:' +
+        app.get('port') + ';press Ctrl+C to terminate.');
 });
