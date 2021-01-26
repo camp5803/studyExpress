d3062d9994f613015036c4fa4bd827f03b4c4c13
camp5803 <hickerd@sch.ac.kr>
2021-01-26 13:35:56 +0900

    study for mariadb

---
+++ mariadb_study/user_info.sql
@@ -0,0 +1,31 @@
+CREATE table user_option(
+    id INT NOT NULL AUTO_INCREMENT,
+    married BOOLEAN,
+    phone_option VARCHAR(13),
+    home_addr VARCHAR(256),
+    class_of TINYINT UNSIGNED,
+    army BOOLEAN,
+    army_disc DATE,
+    CONSTRAINT fid_option FOREIGN KEY(id) REFERENCES user_basic(id) ON DELETE CASCADE,
+    CONSTRAINT pid_option PRIMARY KEY(id)
+)
+COMMENT = "user's option information for signup",
+DEFAULT CHARSET=utf8,
+ENGINE=InnoDB;
+
+CREATE table user_basic (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(20) NOT NULL,
+    passwd VARCHAR(32) NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    sex VARCHAR(6) NOT NULL,
+    age TINYINT UNSIGNED NOT NULL,
+    phonenum VARCHAR(13) NOT NULL,
+    birth DATE NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE INDEX uname (name ASC),
+    UNIQUE INDEX uemail (email ASC))
+COMMENT = 'user information for signup',
+DEFAULT CHARSET=utf8,
+ENGINE=InnoDB;
